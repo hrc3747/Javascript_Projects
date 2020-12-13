b3e5404df7a689126a915c60a56be3d4fc308229
hrc3747 <65324974+hrc3747@users.noreply.github.com>
2020-12-13 13:47:53 -0700

    Upload basic javascript assignment

---
+++ Basic Javascript Projects/Project_1_Expressions_Alert/JS/main.js
@@ -0,0 +1,7 @@
+var Family= "Us", Human = "Renee", Dog = "Abby";     //Defining variables
+var Human = Human.fontcolor("pink");    //Using the fontcolor method on variables
+var Dog = Dog.fontcolor("purple");
+var Family = Family.fontcolor("green");
+var intro = "My name is " + Human +" and my favorite pet is " + Dog + ".";    //Concatenating two variables with strings
+document.write(intro) //Using the document write method on an expression
+window.alert("Hello")   //Using the window alert method on a string
